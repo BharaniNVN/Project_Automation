b91c9050e29eb7d6b7c56f972a085c006f694dc4
BharaniNVN <67556483+BharaniNVN@users.noreply.github.com>
2022-05-23 18:46:52 +0530

    Create Win10O365.ps1

---
+++ AIB/InputTemplates/Win10O365.ps1
@@ -0,0 +1,17 @@
+$buildTimeoutInMinutes=120
+$vmSize="Standard_D2_v2"
+$osDiskSizeGB="127"
+$PUBLISHER='MicrosoftWindowsDesktop'
+$OFFER='office-365'
+$SKU='21h1-evd-o365pp'
+$sigGalleryName= ""
+$sigImageDefName =""
+#$sigImageVersion=1.0.0
+$runOutputName="sigOutput"
+$artifactTags_source=""
+$artifactTags_baseosimg="windows10"
+$imageTemplateName=""
+$OsType="Windows"
+$vnetRgName=""
+$vnetName=""
+$subnetName=""
