6333b4aa00adbef5fcad07ea58b9743419e97c88
Bharani NVN <67556483+BharaniNVN@users.noreply.github.com>
2022-07-07 18:35:44 +0530

    Create kubernetes.tf

---
+++ aks/kubernetes.tf
@@ -0,0 +1,6 @@
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
+}
